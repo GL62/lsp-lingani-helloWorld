15438edb358e2d0dae1037c4e1137966fcf7867c
GL62 <lloydlingani@gmail.com>
2022-09-21 21:04:27 -0400

    Commit for assignment 1.

---
+++ lsp-lingani-helloWorld/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,15 @@
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+/**
+ * Guy L. Lingani
+ * @param args
+ * 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
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Large Scale Programming");
+	}
+
+}
